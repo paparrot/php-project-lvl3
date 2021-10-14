937dd4cc6e9d32dc0167203da081e0c825756143
Klim Subboin <subbotin.klim@gmail.com>
2021-10-14 18:14:41 +0300

    add site status checking

--- app/Http/Controllers/UrlCheckController.php
+++ app/Http/Controllers/UrlCheckController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class UrlCheckController extends Controller
 {
     public function store($id)
     {
-        [$url] = DB::table('urls')->where('id', $id)->get('id');
-
+        $url = DB::table('urls')->where('id', $id)->first();
+        $response = Http::get($url->name);
+        $status = $response->status();
         $data = [
             'url_id' => $url->id,
+            'status_code' => $status,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ];
